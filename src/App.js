a56375dee988185a8125b383206ef49ba811af05
Byrne Hollander <byrnehollander@users.noreply.github.com>
2021-04-26 20:02:17 -0400

    MSG

--- src/App.js
+++ src/App.js
@@ -263,7 +263,7 @@ function App () {
     for (let i = 0; i < winRates.length; i++) {
       arr.push(
         <div key={i} style={{ fontSize: fontSize, marginBottom: 5 }}>
-          <i>{winRates[i].college}</i>: {winRates[i].gihWR} ({winRates[i].count} games)
+          <i>{winRates[i].college}</i>: {winRates[i].gihWR} ({winRates[i].count})
         </div>
       )
     }
@@ -317,7 +317,7 @@ function App () {
             This site uses <Link color='textPrimary' onClick={(event) => event.preventDefault()} href='https://www.17lands.com/card_ratings' target='_blank' rel='noopener noreferrer'>17Lands</Link> data to show how Strixhaven cards perform in each college. This includes splashes – so the "Witherbloom" label also includes decks that are GBw, GBu, etc.
           </Typography>
           <Typography variant='h6' gutterBottom style={{ maxWidth: 950, fontWeight: 400, fontSize: 16 }}>
-            All percentages are for the <b>Games In Hand Win Rate</b> (GIH WR) metric as of April 26, 2021. This is the win rate of games where the card was drawn at some point (including in the opening hand).
+            All percentages are for the <b>Games In Hand Win Rate</b> (GIH WR) metric as of April 26, 2021. This is the win rate of games where the card was drawn at some point (including in the opening hand). The number in parentheses is the number of games used to calculate the win rate (i.e., number of games where the card was ever in the player's hand). There seems to be some consensus that GIH WR is the best metric currently on 17Lands, but note that it is biased towards late game cards.
           </Typography>
           <Typography variant='h6' gutterBottom style={{ maxWidth: 950, fontWeight: 400, fontSize: 16 }}>
             Also note that the collective average win rate from 17Lands users is <b>54.5%</b> (in Strixhaven Premier Draft).
