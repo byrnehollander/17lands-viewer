b4429d46c920f19a2ebc6229990977c37f436344
Byrne Hollander <byrne@arthur.ai>
2021-07-22 19:53:02 -0400

    Update

--- src/aggregateData.js
+++ src/aggregateData.js
@@ -1,18 +1,18 @@
-export const LAST_UPDATED = 'July 19, 2021'
+export const LAST_UPDATED = 'July 22, 2021'
 
 export const WIN_RATES_BY_COLOR = {
-  'Rakdos (BR)': 59.0,
+  'Rakdos (BR)': 59.3,
   'Gruul (RG)': 56.6,
   'Selesnya (GW)': 55.2,
-  'Boros (RW)': 55.4,
-  'Orzhov (WB)': 55.6,
-  'Golgari (BG)': 54.5,
-  'Dimir (UB)': 54.2,
+  'Boros (RW)': 55.1,
+  'Orzhov (WB)': 55.0,
+  'Golgari (BG)': 54.3,
+  'Dimir (UB)': 54.3,
   'Izzet (UR)': 52.2,
-  'Simic (GU)': 52.6,
-  'Azorius (WU)': 48.5
+  'Simic (GU)': 53.4,
+  'Azorius (WU)': 48.9
 }
 
-export const AVERAGE_WIN_RATE = 55.4
+export const AVERAGE_WIN_RATE = 55.5
 
 export const FORMAT_NAME = 'Adventures in the Forgotten Realms'
