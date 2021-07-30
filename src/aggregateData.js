5e9e6dc5f2b9e36eb437be8445863537b7652012
Byrne Hollander <byrne@arthur.ai>
2021-07-30 18:54:33 -0400

    Updates

--- src/aggregateData.js
+++ src/aggregateData.js
@@ -1,16 +1,16 @@
-export const LAST_UPDATED = 'July 25, 2021'
+export const LAST_UPDATED = 'July 30, 2021'
 
 export const WIN_RATES_BY_COLOR = {
-  'Azorius (WU)': 49.1,
-  'Dimir (UB)': 54.1,
-  'Rakdos (BR)': 59.1,
-  'Gruul (RG)': 56.6,
+  'Azorius (WU)': 49.8,
+  'Dimir (UB)': 53.8,
+  'Rakdos (BR)': 58.9,
+  'Gruul (RG)': 56.7,
   'Selesnya (GW)': 55.4,
-  'Orzhov (WB)': 54.6,
-  'Golgari (BG)': 54.3,
-  'Simic (GU)': 53.5,
-  'Izzet (UR)': 51.9,
-  'Boros (RW)': 55.1
+  'Orzhov (WB)': 54.4,
+  'Golgari (BG)': 54.4,
+  'Simic (GU)': 53.4,
+  'Izzet (UR)': 52.3,
+  'Boros (RW)': 54.8
 }
 
 export const AVERAGE_WIN_RATE = 54.8
