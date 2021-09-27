6d9df18668e8d155183e24e772e31cc1ae59db5f
Byrne Hollander <byrne@arthur.ai>
2021-09-27 14:48:32 -0400

    Update

--- src/aggregateData.js
+++ src/aggregateData.js
@@ -1,18 +1,18 @@
-export const LAST_UPDATED = 'September 20, 2021'
+export const LAST_UPDATED = 'September 27, 2021'
 
 export const WIN_RATES_BY_COLOR = {
-  'Azorius (WU)': 60.3,
-  'Dimir (UB)': 60.3,
-  'Rakdos (BR)': 55.5,
-  'Gruul (RG)': 52.2,
-  'Selesnya (GW)': 58.5,
-  'Orzhov (WB)': 58.2,
-  'Golgari (BG)': 56.1,
-  'Simic (GU)': 59.6,
-  'Izzet (UR)': 54.1,
-  'Boros (RW)': 56.2
+  'Azorius (WU)': 59.3,
+  'Dimir (UB)': 59.0,
+  'Rakdos (BR)': 54.8,
+  'Gruul (RG)': 51.7,
+  'Selesnya (GW)': 57.7,
+  'Orzhov (WB)': 56.9,
+  'Golgari (BG)': 55.1,
+  'Simic (GU)': 58.0,
+  'Izzet (UR)': 52.5,
+  'Boros (RW)': 54.7
 }
 
-export const AVERAGE_WIN_RATE = 56.5
+export const AVERAGE_WIN_RATE = 56.0
 
 export const FORMAT_NAME = 'Innistrad: Midnight Hunt'
